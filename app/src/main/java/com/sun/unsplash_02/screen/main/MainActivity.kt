78ce3850d9a53df29bda7b1001b8c68b90ac76a2
luongth-2520 <tran.huu.luong@sun-asterisk.com>
2021-05-24 17:59:47 +0700

    init base

---
+++ app/src/main/java/com/sun/unsplash_02/screen/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sun.unsplash_02.screen.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sun.unsplash_02.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
